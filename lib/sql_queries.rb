55e34d1ded2eeb05203c81af3668f3d77fcc063d
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-21 22:07:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT bears.age, bears.name FROM bears WHERE gender=='F' "
+  "SELECT bears.name, bears.age FROM bears WHERE gender=='F' "
 end
 
 
