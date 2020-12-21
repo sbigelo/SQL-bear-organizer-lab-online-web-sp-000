4103d534398f6209e42787e5198c3f4724e4e3ba
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-21 21:59:41 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 29, "M", "yellow", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 28, "M", "yellow", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 26, "M", "yellow", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 31, "M", "yellow", 1);
+INSERT INTO bears VALUES (5, "Melissa", 236, "F", "green", 0);
+INSERT INTO bears VALUES (6, "Grinch", 12, "M", "orange", 1);
+INSERT INTO bears VALUES (7, "Wendy", 12, "M", "pruple", 1);
+INSERT INTO bears VALUES (8, nil, 13, "F", "yellow", 0);
+
