d59a4c94150efddd01ebd19c681c9a99a6079302
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-21 22:31:12 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 29, "M", "yellow", "mad", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 28, "M", "yellow", "sad", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 26, "M", "yellow", "happy", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 31, "M", "yellow", "wow", 1);
+INSERT INTO bears VALUES (5,"Melissa", 236, "F", "green", "nope", 0);
+INSERT INTO bears VALUES (6,"Grinch", 12, "M", "orange", "oh", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "M", "pruple", "yea", 1);
+INSERT INTO bears VALUES (8,null, 13, "F", "yellow", "ok", 0);
